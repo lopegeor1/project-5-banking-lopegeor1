8d021af3061f6c020ad9b661b6bf5470145efefe
George Lopez <lopegeor@sheridancollege.ca>
2019-07-09 02:52:56 -0400

    test user defined timestamp

--- tests/test_banking.py
+++ tests/test_banking.py
@@ -14,3 +14,13 @@ def test_default_timestamp_now():
     now = dt.datetime.now()
     trans_one = Transaction(100)
     assert trans_one.timestamp >= now
+
+def test_userdefined_timestamp():
+    """
+    Given a user defined input for timestamp, the default current timestamp
+    now() is not used when the Transaction object is created. The user defined
+    timestamp is bound to the Transaction object using self.timestamp = timestamp.
+    """
+    arg_timestamp = dt.datetime(2018, 1, 10)
+    trans_one = Transaction(100, arg_timestamp)
+    assert trans_one.timestamp == arg_timestamp
